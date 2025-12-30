2f43519a62334012b6e797db7c07ce4edfc8ee82
althaf code <afahmd00@gmail.com>
2025-12-30 16:02:54 +0530

    full code

---
+++ chartex.js
@@ -0,0 +1,37 @@
+// int a = 10
+// char b = "A"
+
+
+// var let const 
+
+// var a = 10
+// let b = "sajid"
+// const c = 66.6
+
+
+// var
+var a = 10
+
+var a = 5000  // redecalartion is possible
+a = 7777  // reinislization is possible
+console.log(a);
+
+
+
+let b = 100
+
+// let b = 555
+b = 444
+
+console.log(b);
+
+
+
+// const
+const c = 44444
+// const c = 87788
+// c = 4747747
+console.log(c);
+
+
+
