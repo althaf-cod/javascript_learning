80ca0af1511d3c910ed5e7ced9b67cf7759023cc
althaf code <afahmd00@gmail.com>
2026-01-08 14:23:18 +0530

    eeer

---
+++ ArrayMehtods/array.js
@@ -0,0 +1,25 @@
+const a = 10
+const b = 20
+const c = 30
+
+
+const numbers = [10,20,30]
+
+const fruits = ["orange","grapes","mango"]
+
+fruits.pop()   // to delete last element of array
+fruits.push(33)   // to add last element
+
+fruits.shift()  // to delete first element of an array
+fruits.unshift("althaf")  //to add first elemnt of an array
+
+console.log(fruits); 
+
+
+console.log(fruits[0]);
+console.log(fruits[2]);   // varabileName[indexNumber]
+
+
+
+
+
---
+++ ArrayMehtods/object.js
@@ -0,0 +1,12 @@
+const person = {           //key:value
+    name:"althaf",
+    age:23,
+    job:"develper"
+}
+
+console.log(person.job);   //varaibleName.key
+console.log(person.age);
+
+
+
+
